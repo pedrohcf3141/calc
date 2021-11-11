a9f59ebaf3774384ce6c2920e6d6a3df5a04b4b0
Pedro Henrique Fernandes <pedrohcf3.14@gmail.com>
2021-11-10 23:56:37 -0300

    Deploy

--- edukalculadora/config.py
+++ edukalculadora/config.py
@@ -4,7 +4,7 @@ if ENV == 'dev':
     # SQLALCHEMY_DATABASE_URI ='mysql+pymysql://newuser:password@localhost/calculadora'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///storage.db'
 else:
-    DEBUG = True
+    DEBUG = False
     DATABASE_URL='postgres://nhbzneqiflzggb:b99755d34395cca1275449d2b7fe1f311fcbc191e453ebe2ae7c4c75d04f5a11@ec2-44-199-40-188.compute-1.amazonaws.com:5432/de2megdttiguim'
     SQLALCHEMY_DATABASE_URI = DATABASE_URL.replace("://", "ql://", 1)
 
